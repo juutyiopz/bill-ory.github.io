f17b95a867311d59e29e726f37764b66614c41c9
Your Name <you@example.com>
2019-07-30 22:55:05 +0800

    c

--- aj/lkm-source/README.md
+++
@@ -1,2 +0,0 @@
-# loopback_proxy
-# loopback_proxy
--- aj/lkm-source/udp_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LKM_PROXY_UDP_CLIENT_H_
-#define _LKM_PROXY_UDP_CLIENT_H_
-
-int udp_send_data(struct socket *sock, char *addr, u16 port, void *data, size_t data_len);
-int udp_receive(struct socket *sock, void *buf, size_t buf_len);
-int udp_client_init(struct socket **s);
-
-#endif
-
